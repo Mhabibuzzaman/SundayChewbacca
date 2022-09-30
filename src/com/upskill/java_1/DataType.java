7913bda5243c997f48448363ec4439de5a2bcc1d
Mhabibuzzaman <mhabib_cacc@yahoo.com>
2022-09-29 19:15:53 -0500

    line8

--- src/com/upskill/java_1/DataType.java
+++ src/com/upskill/java_1/DataType.java
@@ -5,7 +5,7 @@ public class DataType {
 	public static void main(String[] args) {
 		System.out.println("Hello World, This is Habib");
 		
-		int a = 5;
+		int a = 6;
 		int b = 10;
 		int c = a+b;
 		System.out.println(c);
